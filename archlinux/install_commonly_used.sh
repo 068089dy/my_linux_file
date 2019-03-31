c1044735ab73fb7000bd7c891667cfee5cf231d0
068089dy <068089dy@gmail.com>
2019-03-31 11:15:25 +0800

    first

---
+++ archlinux/install_commonly_used.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo pacman -S visual-studio-code telegram-desktop kdenlive obs-studio firefox \	google-chrome htop nmap net-tools ibus ibus-google-pinyin vim zsh \
+	flameshot wps-office terminator netease-cloud-music
