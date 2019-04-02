6326150f14c52469e64f1401231318e94c3300c0
068089dy <068089dy@gmail.com>
2019-04-02 18:36:17 +0800

    Update install_commonly_used.sh

--- archlinux/install_commonly_used.sh
+++ archlinux/install_commonly_used.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-sudo pacman -S visual-studio-code telegram-desktop kdenlive obs-studio firefox \	google-chrome htop nmap net-tools ibus ibus-google-pinyin vim zsh \
-	flameshot wps-office terminator netease-cloud-music
+sudo pacman -S visual-studio-code telegram-desktop kdenlive obs-studio firefox \	
+	google-chrome htop nmap net-tools ibus ibus-google-pinyin vim zsh \
+	flameshot wps-office terminator netease-cloud-music v2ray proxychains \
+	vlc
+# coding
+sudo pacman -S workbetch pycharm 
