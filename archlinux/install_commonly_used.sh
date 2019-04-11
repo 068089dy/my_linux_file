aaca00d2fdad2f3cf76bfeba3d9df36295fef490
068089dy <068089dy@gmail.com>
2019-04-11 18:37:45 +0800

    Update install_commonly_used.sh

--- archlinux/install_commonly_used.sh
+++ archlinux/install_commonly_used.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
-sudo pacman -S visual-studio-code telegram-desktop kdenlive obs-studio firefox \	
-	google-chrome htop nmap net-tools ibus ibus-google-pinyin vim zsh \
-	flameshot wps-office terminator netease-cloud-music v2ray proxychains \
-	vlc
+sudo pacman -S wget terminator visual-studio-code-bin telegram-desktop kdenlive obs-studio firefox google-chrome htop nmap net-tools ibus ibus-google-pinyin vim zsh flameshot wps-office terminator netease-cloud-music v2ray proxychains vlc
+
 # coding
-sudo pacman -S workbetch pycharm 
+sudo pacman -S mysql-workbench pycharm-community-edition redis-desktop-manager
+# watch shell
+# cat /etc/shells
+
+# install on-my-zsh
+wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh
+# change shell
+chsh -s /bin/zsh
+
+# 
+sudo pacman -S redis mariadb
+systemctl start reids
+sudo mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
+systemctl start mariadb
+mysql_secure_installation
+systemctl restart mariadb
